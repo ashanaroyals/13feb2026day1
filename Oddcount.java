33dd769f0e56d40029da6ac3c2cf50131a9a6c2b
K G Ashalatha <kgashalatha3@gmail.com>
2026-02-16 11:23:45 +0530

    Add files via upload

---
+++ Oddcount.java
@@ -0,0 +1,16 @@
+import java .util.Scanner;
+class  Oddcount
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int count=0;
+for(int i=1;i<=a;i++)
+{
+if(i%2==0)
+count++;
+}
+System.out.println(count);
+}
+}
